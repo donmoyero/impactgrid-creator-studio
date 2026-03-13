2af7b3e67297638c5245a1892e346b022c113a75
donmoyero <63630549+donmoyero@users.noreply.github.com>
2026-03-13 11:36:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -20,13 +20,24 @@ let roomBox = document.getElementById("room");
 if(room === "Fitness Studio"){
 roomBox.style.backgroundImage = "url('https://images.unsplash.com/photo-1517836357463-d25dfeac3438')";
 }
-
 else if(room === "Data Lab"){
 roomBox.style.backgroundImage = "url('https://images.unsplash.com/photo-1551288049-bebda4e38f71')";
 }
-
 else{
 roomBox.style.backgroundImage = "url('https://images.unsplash.com/photo-1492724441997-5dc865305da7')";
+}
+
+// set avatar
+let avatar = document.getElementById("agentAvatar");
+
+if(agent === "DIJO Coach"){
+avatar.src = "https://cdn-icons-png.flaticon.com/512/1995/1995574.png";
+}
+else if(agent === "DIJO Analyst"){
+avatar.src = "https://cdn-icons-png.flaticon.com/512/3135/3135715.png";
+}
+else{
+avatar.src = "https://cdn-icons-png.flaticon.com/512/4140/4140048.png";
 }
 
 }

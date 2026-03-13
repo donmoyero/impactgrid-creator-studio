e4fffbc9e81bc9508e1d89fe25754a93635f6a1f
donmoyero <63630549+donmoyero@users.noreply.github.com>
2026-03-13 11:02:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function generate(){
 
+let topic = document.getElementById("topic").value;
+
+let script = "Today we are talking about " + topic + ". This is a powerful topic you should understand.";
+
+document.getElementById("output").innerHTML = script;
+
+}
